c166397debbdfdaee782b725b6f2800813382408
shumilovDaniil <shumilov.daniil.s@gmail.com>
2021-07-23 20:59:15 +0500

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ let path = {
   build: {
     html: project_folder + "/",
     css: project_folder + "/css/",
+    js: project_folder + "/js/",
     img: project_folder + "/img/",
     fonts: project_folder + "/fonts/",
   },
@@ -14,6 +15,7 @@ let path = {
   src: {
     html: source_folder + "/*.html",
     css: source_folder + "/scss/*.scss",
+    js: source_folder + "/js/*.js",
     img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
     fonts: source_folder + "/fonts/",
   },
@@ -21,6 +23,7 @@ let path = {
   watch: {
     html: source_folder + "/**/*.html",
     css: source_folder + "/scss/**/*.scss",
+    js: source_folder + "/js/**/*.js",
     img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
   },
   clean: "./" + project_folder + "/"
@@ -51,47 +54,40 @@ function html() {
 
 function css() {
   return src(path.src.css)
-    .pipe(
-      scss({
-        outputStyle: "expanded"
-      })
+  .pipe(
+    scss({
+      outputStyle: "expanded"
+    })
     )
     .pipe(
       autoprefixer({
         overrideBrowserlist: ["last 5 versions"],
         cascade: true
       })
-    )
-    .pipe(dest(path.build.css))
+      )
+      .pipe(dest(path.build.css))
+      .pipe(browsersync.stream())
+}
+    
+function js() {
+  return src(path.src.js)
+    .pipe(dest(path.build.js))
     .pipe(browsersync.stream())
 }
 
 function watchFiles(params) {
   gulp.watch([path.watch.html], html);
   gulp.watch([path.watch.css], css);
-  // gulp.watch([path.watch.img], images);
+  gulp.watch([path.watch.js], js);
 }
 
-// function images() {
-//   return src(path.src.img)
-//     .pipe(
-//       imagemin({
-//         progressive: true,
-//         svgoPlugins: [{ remoteViewBox: false }],
-//         interlaced: true, 
-//         optimizationLevel: 3, // 0 to 7
-//       })
-//     )
-//     .pipe(dest(path.build.img))
-//     .pipe(browsersync.stream())
-// }
 
-let build = gulp.series(gulp.parallel(css, html));
+let build = gulp.series(gulp.parallel(css, html, js));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
-// exports.images = images;
 exports.css = css;
 exports.html = html;
+exports.js = js;
 exports.build = build;
 exports.watch = watch;
 exports.default = watch;
